f8fb5cf9b7344e402d80934ada276c10bb9e0a7a
Haas Timon <haast@in.tum.de>
2020-12-05 14:40:47 +0100

    Update Test2.cs

--- Assets/Scripts/Test2.cs
+++ Assets/Scripts/Test2.cs
@@ -7,7 +7,7 @@ public class Test2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Servus
     }
 
     // Update is called once per frame
