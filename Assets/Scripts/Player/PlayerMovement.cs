d926ff79e9ce13a4f7fe0292bddae0efbbb0460a
Julian Z <julian.z@gmx.net>
2020-12-05 14:44:25 +0100

    TestScene

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector2 direction;
+    [SerializeField]
+    private float speed = 10;
+    private Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        direction = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = (direction * speed);
+    }
+}
