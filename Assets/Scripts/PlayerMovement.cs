03e54be2d098e7369788db7e0a2849379984fb08
AnianTUM <75169956+AnianTUM@users.noreply.github.com>
2020-12-28 18:36:05 +0100

    Scenenwechsel lab <-> city

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -13,8 +15,10 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        gameObject.transform.position = GameData.Instance.setGetlastRoom == GameData.LastRoom.Start ? new Vector3(-6.14f,1.77f,0) : new Vector3(46.74f,-13,0);
         rb = this.GetComponent<Rigidbody2D>();
         direction = Vector2.zero;
+        GameData.Instance.setGetlastRoom = GameData.LastRoom.Lab;
     }
 
     // Update is called once per frame
@@ -26,5 +30,13 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         rb.velocity = (direction * speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag.Equals("ScenenWechsel"))
+        {
+            SceneManager.LoadScene("City");
+        }
     }
 }
