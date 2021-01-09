d0f5f565b83e79abaf5469077a6d5abcc19a37db
Haas Timon <haast@in.tum.de>
2021-01-09 18:56:52 +0100

    Added Camera Bounding Box

---
+++ Assets/Scripts/Scenes/Temp/CameraFollow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Room room;
+    
+    private Vector2 camHalfDim;
+    private Camera cam;
+
+    private bool _atLeftBorder, _atRightBorder, _atTopBorder, _atBottomBorder;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camHalfDim = new Vector2(1.75f, 1f);
+        cam = GetComponent<Camera>();
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(room)
+        {
+            _atLeftBorder = (player.position.x - (camHalfDim.x * cam.orthographicSize)) < room.center.x - (room.width / 2);
+            _atRightBorder = (player.position.x + (camHalfDim.x * cam.orthographicSize)) > room.center.x + (room.width / 2);
+            _atTopBorder = (player.position.y + (camHalfDim.y * cam.orthographicSize)) > room.center.y + (room.height / 2);
+            _atBottomBorder = (player.position.y - (camHalfDim.y * cam.orthographicSize)) < room.center.y - (room.height / 2);
+            if ((_atLeftBorder || _atRightBorder) && (_atTopBorder || _atBottomBorder))
+                transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            else if (_atLeftBorder || _atRightBorder)
+                transform.position = new Vector3(transform.position.x, player.position.y, transform.position.z);
+            else if (_atTopBorder || _atBottomBorder)
+                transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+            else
+                transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/Scenes/Temp/Room.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    [Range(0.0f, 60.0f)]
+    public float width = 8;
+    [Range(0.0f, 50.0f)]
+    public float height = 4;
+
+    [HideInInspector]
+    public Vector3 center;
+
+    private void Start()
+    {
+        center = transform.position;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(width, height, 1));
+    }
+}
