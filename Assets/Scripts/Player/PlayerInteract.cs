3c12c657daa384b69d48d7551400b2340df1359a
Julian Z <julian.z@gmx.net>
2021-01-09 17:53:03 +0100

    Folder Structure

---
+++ Assets/Scripts/Player/PlayerInteract.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInteract : MonoBehaviour
+{
+    
+    public Image interactPrompt;
+    
+    private bool _canInteract;
+    private Interactable _interactable;
+    
+    private void Start()
+    {
+        interactPrompt.enabled = false;
+    }
+    
+    private void Update()
+    {
+        if (_canInteract && _interactable && Input.GetButtonDown("Interact") )
+        {
+            Input.ResetInputAxes();
+            _canInteract = false;
+            interactPrompt.enabled = false;
+            _interactable.Action(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Interactable"))
+        {
+            interactPrompt.enabled = true;
+            _interactable = other.GetComponent<Interactable>();
+            _interactable.highlight.SetActive(true);
+            _canInteract = true;
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Interactable"))
+        {
+            interactPrompt.enabled = false;
+            _interactable.highlight.SetActive(false);
+            _interactable = null;
+            _canInteract = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Scenes/Laboratory/Player/PlayerPositioning.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPositioning : MonoBehaviour
+{
+    
+    
+    private void Start()
+    {
+        gameObject.transform.position = GameData.Instance.setGetlastRoom == GameData.LastRoom.Start ? new Vector3(-6.14f,1.77f,0) : new Vector3(46.74f,-13,0);
+        GameData.Instance.setGetlastRoom = GameData.LastRoom.Lab;
+    }
+
+    
+}
