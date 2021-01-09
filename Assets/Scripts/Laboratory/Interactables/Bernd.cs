3738b10a08ad23bdcc7e1d677554902062336f18
AnianTUM <75169956+AnianTUM@users.noreply.github.com>
2021-01-09 15:03:25 +0100

    Teile von Bernd

---
+++ Assets/Scripts/Laboratory/Interactables/Bernd.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bernd : Interactable
+{
+    
+    public MessageBox messageBox;
+    void Update()
+    {
+        if (Active && !messageBox.GetMessageActive())
+        {
+            UndoAction();
+        }
+    }
+
+    protected override void SpecificAction()
+    {
+        LinkedList<string> authors = new LinkedList<string>();
+        authors.AddLast("Jordan");
+        authors.AddLast("Bernd");
+        authors.AddLast("Jordan");
+        LinkedList<string> messages = new LinkedList<string>();
+        messages.AddLast("jo du wichser wegen dir bin ich hier");
+        messages.AddLast("halts maul wir haben größere Probleme, wir kommen hier nie wieder weg deswegen bring ich dich jetzt um");
+        messages.AddLast("oh nein jetzt bin ich gezwungen dich zu erschießen");
+        messageBox.ShowMessages(authors, messages);
+    }
+
+    protected override void UndoSpecificAction()
+    {
+        
+    }
+
+   
+}
