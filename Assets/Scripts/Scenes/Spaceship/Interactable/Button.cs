937d4ac65ebbbf45ef2a8e6d6f099bad63b29ff8
Julian Z <julian.z@gmx.net>
2021-01-17 23:07:18 +0100

    Ending Decision

--- Assets/Scripts/Scenes/Spaceship/Interactable/Button.cs
+++ Assets/Scripts/Scenes/Spaceship/Interactable/Button.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Button : Interactable
 {
     
     [SerializeField] private MessageBox messageBox;
+    [SerializeField] private Image stayButton;
+    [SerializeField] private Image leaveButton;
+    [SerializeField] private Image deferButton;
     private bool _readEnd;
     
     protected override void SpecificAction()
@@ -14,37 +18,71 @@ public class Button : Interactable
 
     protected override void UndoSpecificAction()
     {
+        LinkedList<string> messages = new LinkedList<string>();
+        messages.AddLast("Das Schiff scheint noch überraschend intakt zu sein... Ich hätte jetzt die Gelegenheit, von diesem Planeten zu fliehen. Aber wohin?");
+        messages.AddLast("Was soll ich nur tun? Ich bin der einzige Überlebende... die Erde ist tot... meine Familie ist irgendwo da draußen...");
+        messages.AddLast("Soll ich versuchen, sie zu finden? Wer weiß, ob sie überhaupt noch leben? Oder bereits aus dem Kälteschlaf erwacht sind?");
         if (GameData.Instance.WasInChurch)
         {
-            string[] messages = {
-                "Das Schiff scheint noch überraschend intakt zu sein... Ich hätte jetzt die Gelegenheit, von diesem Planeten zu fliehen. Aber wohin?",
-                "Was soll ich nur tun? Ich bin der einzige Überlebende... die Erde ist tot... meine Familie ist irgendwo da draußen...",
-                "Soll ich versuchen, sie zu finden? Wer weiß, ob sie überhaupt noch leben? Oder bereits aus dem Kälteschlaf erwacht sind?",
-                "Oder soll ich hierbleiben? Ich verdanke den Rebellen mein Leben. Möglicherweise kann ich ihnen wirklich helfen...",
-                "Vielleicht finde ich in ihnen sogar eine neue Familie? Ein neues Leben? Wer weiß, ob ich je wieder einen so bewohnbaren Planeten finde?"
-            };
-            messageBox.ShowMonologue("Jordan", new LinkedList<string>(messages));
-            _readEnd = true;
+            messages.AddLast("Oder soll ich hierbleiben? Ich verdanke den Rebellen mein Leben. Möglicherweise kann ich ihnen wirklich helfen...");
+            messages.AddLast("Vielleicht finde ich in ihnen sogar eine neue Familie? Ein neues Leben? Wer weiß, ob ich je wieder einen so bewohnbaren Planeten finde?");
         }
-        else
-        {
-            messageBox.ShowMessage("Jordan", "Ich möchte mich hier lieber noch etwas umsehen, bevor ich das Schiff starte.");
-        }
+        messageBox.ShowMonologue("Jordan", messages);
+        
+        _readEnd = true;
     }
 
     protected override void SpecificUpdate()
     {
         if (_readEnd && !messageBox.GetMessageActive())
         {
-            Ending();
+            SetPlayerControls(false);
+            SetButtons(true, GameData.Instance.WasInChurch);
             _readEnd = false;
         }
     }
 
-    private void Ending()
+    #region Helper Functions
+    
+    private void SetButtons(bool active, bool includeStayButton)
     {
-        // TODO: add decision & end to demo
-        Debug.Log("End: Decision");
+        leaveButton.gameObject.SetActive(active);
+        if (includeStayButton)
+            stayButton.gameObject.SetActive(active);
+        deferButton.gameObject.SetActive(active);
     }
+    
+    private void SetPlayerControls(bool active)
+    {
+        PlayerMovement.CanMove = active;
+        PlayerInteract.CanInteract = active;
+        PlayerShoot.AllowInput = active;
+    }
+    
+    #endregion
+
+    #region Button Functions
+    
+    public void Stay()
+    {
+        Debug.Log("End: Stay");
+        SetPlayerControls(true);
+        SetButtons(false, true);
+    }
+    
+    public void Leave()
+    {
+        Debug.Log("End: Leave");
+        SetPlayerControls(true);
+        SetButtons(false, true);
+    }
+
+    public void Defer()
+    {
+        SetPlayerControls(true);
+        SetButtons(false, true);
+    }
+    
+    #endregion
     
 }
