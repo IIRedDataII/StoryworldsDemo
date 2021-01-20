9091150b4d8995b73e0633dd156a833083a8944c
Haas Timon <haast@in.tum.de>
2021-01-20 11:05:31 +0100

    New Projectile for Wardens

---
+++ Assets/Scripts/Scenes/Laboratory/Aliens/WardenProjectile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WardenProjectile : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerDeath>()?.kill();
+            Destroy(gameObject);
+        }
+        else if(other.CompareTag("Warden"))
+        {
+            return;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
