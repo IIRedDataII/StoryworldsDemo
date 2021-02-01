22f89e8b24ce0707ad2422de6bbf1467d1c1897d
Julian Z <julian.z@gmx.net>
2021-02-01 13:50:01 +0100

    Menu Scene

---
+++ Assets/Scripts/Scenes/Menu/Menu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+
+    public GameObject controls;
+
+    public void ToggleControls()
+    {
+        controls.SetActive(!controls.activeSelf);
+    }
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Laboratory");
+    }
+    
+}
